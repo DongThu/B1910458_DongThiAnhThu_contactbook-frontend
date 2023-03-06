fb8a9350044fdb3281756ebd5f79cd3c3b6a4be1
DongThu <thub1910458@student.ctu.edu.vn>
2023-03-06 10:38:50 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+
+}
+</script>
+            npm run dev
+<template>
+  <header>
+    <h1>Hello, Vue.js!</h1>
+  </header>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
