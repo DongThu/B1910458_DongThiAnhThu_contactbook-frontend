675d86234d119125ede2f7494ffc1f0d3a59b9cd
DongThu <thub1910458@student.ctu.edu.vn>
2023-03-22 22:07:44 +0700

    Cai dat trang loi 404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,8 @@
-<!-- <template>
+<template>
   <div class="page">
     <p>
       Oops, không thể tìm thấy trang. Trở về
       <router-link to="/">trang chủ.</router-link>
     </p>
   </div>
-</template> -->
+</template>
